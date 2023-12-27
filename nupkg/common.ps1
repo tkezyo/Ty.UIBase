79d5fd0c0872cb430312bd1a3907e9ce5c15db23
金俊成 <tkezyo@live.com>
2023-12-27 20:13:25 +0800

    init

---
+++ nupkg/common.ps1
@@ -0,0 +1,15 @@
+# Paths
+$packFolder = (Get-Item -Path "./" -Verbose).FullName
+$rootFolder = Join-Path $packFolder "../"
+
+# List of solutions
+$solutions = (
+    "basics"
+)
+
+# List of projects
+$projects = (
+    "src/Ty.UIBase",
+    "src/Ty.WPFBase"
+   
+)
---
+++ src/Ty.UIBase/PageOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Ty;
+
+public class PageOptions
+{
+    /// <summary>
+    /// 登录页面
+    /// </summary>
+    public Type? FirstLoadPage { get; set; }
+    /// <summary>
+    /// 布局页面
+    /// </summary>
+    public Type? LayoutPage { get; set; }
+    public string? Title { get; set; }
+}
