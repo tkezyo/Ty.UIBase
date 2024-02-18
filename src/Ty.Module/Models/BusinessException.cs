7bb3b3f04f955f7bdca237ffef61ac759801bc66
金俊成 <tkezyo@live.com>
2024-02-18 21:54:17 +0800

    添加namevalue

---
+++ src/Ty.Module/Models/BusinessException.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Ty;
+
+[Serializable]
+public class BusinessException : Exception
+{
+    public string? Code { get; set; }
+
+    public string? Details { get; set; }
+
+    public LogLevel LogLevel { get; set; }
+
+    public BusinessException(string? code = null, string? message = null, string? details = null, Exception? innerException = null, LogLevel logLevel = LogLevel.Warning)
+        : base(message, innerException)
+    {
+        Code = code;
+        Details = details;
+        LogLevel = logLevel;
+    }
+
+    public BusinessException WithData(string name, object value)
+    {
+        Data[name] = value;
+        return this;
+    }
+}
---
+++ src/Ty.Module/Models/DisposeAction.cs
@@ -0,0 +1,55 @@
+﻿namespace Ty;
+
+/// <summary>
+/// This class can be used to provide an action when
+/// Dispose method is called.
+/// </summary>
+public class DisposeAction : IDisposable
+{
+    private readonly Action _action;
+
+    /// <summary>
+    /// Creates a new <see cref="DisposeAction"/> object.
+    /// </summary>
+    /// <param name="action">Action to be executed when this object is disposed.</param>
+    public DisposeAction(Action action)
+    {
+        _action = action;
+    }
+
+    public void Dispose()
+    {
+        _action();
+    }
+}
+
+/// <summary>
+/// This class can be used to provide an action when
+/// Dispose method is called.
+/// <typeparam name="T">The type of the parameter of the action.</typeparam>
+/// </summary>
+public class DisposeAction<T> : IDisposable
+{
+    private readonly Action<T> _action;
+
+    private readonly T? _parameter;
+
+    /// <summary>
+    /// Creates a new <see cref="DisposeAction"/> object.
+    /// </summary>
+    /// <param name="action">Action to be executed when this object is disposed.</param>
+    /// <param name="parameter">The parameter of the action.</param>
+    public DisposeAction(Action<T> action, T parameter)
+    {
+        _action = action;
+        _parameter = parameter;
+    }
+
+    public void Dispose()
+    {
+        if (_parameter != null)
+        {
+            _action(_parameter);
+        }
+    }
+}
